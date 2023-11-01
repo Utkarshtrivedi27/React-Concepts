0468b77f14a187d901532997e3c9c4ee9cef172d
Utkarsh Trivedi <79292743+Utkarshtrivedi27@users.noreply.github.com>
2023-11-01 22:52:06 +0530

    Create WarningButton.js

---
+++ Functional_Component_Demo/WarningButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import CustomButton from './CustomButton';
+
+function WarningButton() {
+  // return React.createElement(CustomButton, {color: 'red'}, null);
+  return <CustomButton color="red" />;        // JSX code
+}
+
+export default WarningButton;
